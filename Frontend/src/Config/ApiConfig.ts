47cc0f902a2cc1317a3435599ac62ec979afcc68
Nibedan <nibedanpati@gmail.com>
2025-01-26 14:56:16 +0530

    api-config.ts chnanged

--- Frontend/src/Config/ApiConfig.ts
+++ Frontend/src/Config/ApiConfig.ts
@@ -1,5 +1,5 @@
 // const API_BASE_URL = 'http://localhost:5000/api/v1'; // development URL
 
-const API_BASE_URL ="budgetmap-production.up.railway.app/api/v1" // Production URL
+const API_BASE_URL ="https://budgetmap-production.up.railway.app/api/v1" // Production URL
 
 export default API_BASE_URL
